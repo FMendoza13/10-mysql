3955c008add74030e3c879e766cc1552b9f79668
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-12-16 21:32:07 -0800

    created bamazon sql database

---
+++ bamazon.sql
@@ -0,0 +1,17 @@
+
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+ USE bamazon;
+
+ CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name TEXT NOT NULL,
+    department_name TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
