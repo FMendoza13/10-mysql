1ff30fc0a9e4ff181d5028019b9960ad1a91c0e3
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-12-17 18:22:14 -0800

    shows items to user on program load, also npm chalk install

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 
 var mysql = require('mysql');
-
 var inquirer = require('inquirer');
+var chalk = require('chalk');
+
 
 var connection = mysql.createConnection({
   
@@ -15,4 +16,18 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connection successful');
 
+    start();
  });
+
+ var displayItems = function() {
+    connection.query('SELECT * FROM products', (err, res) => {
+        // console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log(chalk.blue.bold(`\n\tItem ID: ${res[i].item_id}\n\tProduct Name: ${res[i].product_name}\n\tPrice: $${res[i].price}\n\n--------------------------------`));
+        }
+    });
+};
+
+var start = function() {
+    displayItems();
+}; 
